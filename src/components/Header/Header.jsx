5b7579a51504caeb4d02ed5ec208961aabcd9dd8
Andy Ramaroson <andyramaroson@gmail.com>
2023-06-09 12:07:48 +0200

    (feat) navbar, header scss remake responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,57 @@
 import React from 'react'
 import desktop from './../../assets/desktop.jpg'
-import IconArrow from '../Icons/arrow'
 import './Header.scss'
 
 function Header() {
+  //   return (
+  //     <div className="row m-0">
+  //       <div className="col-12 col-xl-5 mt-4">
+  //         <div className="row justify-content-center p-4 ">
+  //           <div className="col-0 col-xl-2"></div>
+  //           <div className="col-12 col-xl-8 pt-4 ">
+  //             <h1 className="main-title">
+  //               Création <span className="green">&</span>
+  //               <br /> Développement web <br /> par{' '}
+  //               <span className="green">Lemurian Agency</span>
+  //             </h1>
+  //             <p className="lead mt-2 mb-3 content">
+  //               Lemurian Agency développe des outils sur-mesure à destination des
+  //               entreprises. Je fais un sorte de faire gagner du temps et de
+  //               l'efficacité à mes clients en numérisant leurs processus, dans le
+  //               respect de leurs règles métiers.
+  //             </p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <div className="col-0 col-xl-5">
+  //         <img className="img-header" src={desktop} alt="Lemurian Agency" />
+  //       </div>
+  //     </div>
+  //   )
+
   return (
-    // <header className="mb-5">
-    <div className="row m-0">
-      <div className="col-12 col-xl-7 mt-4">
-        <div className="row justify-content-center p-4">
-          <div className="col-0 col-xl-2"></div>
-          <div className="col-12 col-xl-6 mt-4 pt-2 ">
-            <h1>
-              Création <span className="green">&</span> Développement web par
-              Lemurian Agency
-            </h1>
-            <p className="lead mt-3 mb-2">
-              Lemurian Agency développe des outils sur-mesure à destination des
-              entreprises. Je fait un sorte de faire gagner du temps et de
-              l'efficacité à nos clients en numérisant leurs processus, dans le
-              respect de leurs règles métiers.
-            </p>
-            {/* <div className="col-12 mt-5">
-              <a href="#main">
-                <div className="ico animated">
-                  <div className="circle circle-top"></div>
-                  <div className="circle circle-main"></div>
-                  <div className="circle circle-bottom"></div>
-                  <IconArrow />
-                </div>
-              </a>
-            </div> */}
-          </div>
-        </div>
+    <section className="header">
+      <div className="container__left">
+        <h1 className="container__left__title">
+          Création <span className="green">&</span>
+          <br /> Développement web <br /> par{' '}
+          <span className="green">Lemurian Agency</span>
+        </h1>
+        <p className="container__left__text">
+          Lemurian Agency développe des outils sur-mesure à destination des
+          entreprises. Je fais un sorte de faire gagner du temps et de
+          l'efficacité à mes clients en numérisant leurs processus, dans le
+          respect de leurs règles métiers.
+        </p>
       </div>
-      <div className="col-0 col-xl-5">
-        <img className="img-header" src={desktop} alt="Lemurian Agency" />
+      <div className="container__right">
+        <img
+          className="container__Right__img"
+          src={desktop}
+          alt="Lemurian Agency"
+        />
       </div>
-    </div>
-    // </header>
+    </section>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,46 +13,8 @@ const Navbar = () => {
         <h2 className="avatar">Lemurian Agency</h2>
       </Link>
 
-      <div className="d-none d-md-block">
-        <div className="d-flex align-items-center justify-content-between">
-          <Link to="/" className="navbar-lemurian-item navbar-lemurian-link">
-            Home
-          </Link>
-          <Link to="/" className="navbar-lemurian-item navbar-lemurian-link">
-            Expertise
-          </Link>
-          <Link
-            to="/expertise"
-            className="navbar-lemurian-item navbar-lemurian-link"
-          >
-            Méthodologie
-          </Link>
-          <Link
-            to="/Méthodologie"
-            className="navbar-lemurian-item navbar-lemurian-link"
-          >
-            Projet
-          </Link>
-
-          {userSignedIn ? (
-            <Link
-              to="/logout"
-              className="navbar-lemurian-item navbar-lemurian-link"
-            >
-              Se déconnecter
-            </Link>
-          ) : (
-            <Link
-              to="/login"
-              className="navbar-lemurian-item navbar-lemurian-link"
-            >
-              Se connecter
-            </Link>
-          )}
-        </div>
-      </div>
-
-      <div className="d-block d-md-none">
+      <div className="d-md-block">
+        {/* <div className="d-block d-md-none"> */}
         <div className="navbar-dropdown">
           <input id="toggle" type="checkbox" />
           <label className="hamburger" htmlFor="toggle">
