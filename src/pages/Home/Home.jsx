2812f43a9648d602afad0b93aca88ca6c7626ae2
Andy Ramaroson <andyramaroson@gmail.com>
2023-06-21 21:30:11 +0200

    (feat) add blockText

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Header from './../../components/Header/Header'
-// import BlocText from '../../components/BlocText/BlocText'
+import BlocText from '../../components/BlocText/BlocText'
 import './Home.scss'
-
 
 function Home() {
   return (
     <section>
       <Header />
-      {/* <BlocText/>     */}
+      <BlocText />
       <h2>Notre Expertise</h2>
-      <p >Je réalise depuis 3 ans des outils sur-mesure à destination d'entreprises de petite taille. Je transforme chaque besoin de mes clients en solution web, en garantissant le respect de leurs règles métiers.</p>
+      <p>
+        Je réalise depuis 3 ans des outils sur-mesure à destination
+        d'entreprises de petite taille. Je transforme chaque besoin de mes
+        clients en solution web, en garantissant le respect de leurs règles
+        métiers.
+      </p>
     </section>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
